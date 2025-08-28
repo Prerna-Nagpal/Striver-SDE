541766c61df6968f43cbb368ba058f00346cb28c
Prerna-Nagpal <140643802+Prerna-Nagpal@users.noreply.github.com>
2025-08-29 00:19:38 +0530

    Time: 0 ms (100%), Space: 13.4 MB (42.81%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -7,12 +7,11 @@ public:
         while(low<=high){
             int mid = (low+high)/2;
             int row = mid/m;
-            int col=mid%m;
+            int col = mid%m;
             if(matrix[row][col]==target) return true;
             else if(matrix[row][col]<target) low=mid+1;
-            else high=mid-1;
+            else high = mid-1;
         }
         return false;
-        
     }
 };
