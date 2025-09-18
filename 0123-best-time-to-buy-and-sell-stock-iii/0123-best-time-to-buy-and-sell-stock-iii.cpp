037a0f94e59a9a9ef37f8a7d621a28c1be9d0266
Prerna-Nagpal <140643802+Prerna-Nagpal@users.noreply.github.com>
2025-09-18 17:38:18 +0530

    Time: 65 ms (66.11%), Space: 79.3 MB (90.24%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         vector<vector<int>> ahead(2, vector<int>(3, 0));
+    vector<vector<int>> cur(2, vector<int>(3, 0));
+int n = prices.size();
+    for (int ind = n - 1; ind >= 0; ind--) {
+        for (int buy = 0; buy <= 1; buy++) {
+            for (int cap = 1; cap <= 2; cap++) {
+                if (buy == 0) { // We can buy the stock
+                    cur[buy][cap] = max(0 + ahead[0][cap], 
+                                        -prices[ind] + ahead[1][cap]);
+                }
+
+                if (buy == 1) { // We can sell the stock
+                    cur[buy][cap] = max(0 + ahead[1][cap],
+                                        prices[ind] + ahead[0][cap - 1]);
+                }
+            }
+        }
+        // Update the ahead state with the current state for the next iteration.
+        ahead = cur;
+    }
+
+    return ahead[0][2];
+    }
+};
